62224f1609d6f726ab984c4e49fbdf35c97671c3
Mikko Juola <mikjuo@gmail.com>
2022-08-08 17:24:30 -0700

    Add a new include dir for Eigen.

--- build.rs
+++ build.rs
@@ -9,6 +9,7 @@ fn main() {
         .flag("-I/usr/local/include/eigen3")
         .flag("-I/usr/local/include/libcmaes")
         .flag("-I/opt/homebrew/Cellar/eigen/3.3.9/include/eigen3")
+        .flag("-I/opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3")
         .flag("-std=c++11")
         .compile("librcmaesglue.a");
     // Hack to convince rust to link to cmaes with Cargo.
