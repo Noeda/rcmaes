b9b3ddb20afc27cf0610e75960262f5d13e1f08d
Mikko Juola <mikko.juola@iki.fi>
2025-10-07 20:25:11 -0700

    Move to `pkg-config` based finding of libcmaes and eigen3.
    I had previously just hardcoded horrible homebrew paths and stuff.
    
    On my machines, this pkg-config is working, although I had to add a
    small hack for libcmaes, because the include path as given by pkg-config
    does not seem like it is usable directly (it gives a path that's too
    deep by one directory, so I had to put ../ on it).
    
    This worked on MacOS and Linux. libcmaes tends to be too obscure to be
    packaged by distributions, but a locally installed one will do (it
    includes a pkg-config .pc file). Eigen3 worked with the pkg-config
    without other fuss.

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 extern crate cc;
+extern crate pkg_config;
 
 fn main() {
     let mut cc_build = cc::Build::new();
@@ -9,23 +10,24 @@ fn main() {
     // of laziness, and then adding more paths as time marches on
     // and the code breaks.
 
-    cc_build
-        .cpp(true)
-        .file("src/c/cmaes.cc")
-        .flag("-I/usr/include/eigen3")
-        .flag("-I/usr/include/libcmaes")
-        .flag("-I/usr/local/include/eigen3")
-        .flag("-I/usr/local/include/libcmaes")
-        .flag("-I/usr/local/include")
-        .flag("-I/opt/homebrew/opt/libomp/include")
-        .flag("-I/opt/homebrew/Cellar/eigen/3.3.9/include/eigen3")
-        .flag("-I/opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3")
-        .flag("-std=c++11");
+    let cmaes = pkg_config::probe_library("libcmaes").unwrap();
+    pkg_config::probe_library("eigen3").unwrap();
 
-    if let Ok(value) = std::env::var("HOME") {
-        cc_build.flag(format!("-I{}/.local/include", value));
+    cc_build.cpp(true).file("src/c/cmaes.cc").flag("-std=c++11");
+
+    // 7 Oct 2025: When I was testing the pkg-config on MacOS, I noticed this:
+    //
+    // The pkg-config for libcmaes seems to give a directory without "libcmaes/" root,
+    // but libcmaes' own headers are anticipating "libcmaes/" to be present.
+    //
+    // We "solve" this by unceremoniously slapping "../" to our path we get from pkg-config.
+    // I added both the path as given by pkg-config, and also the ../
+    // This may or may not cause horrible problems down the line, but considering how the headers
+    // were found before I did pkg-config at all, it's _maybe_ an improvement. Maybe.
+    for incl_path in cmaes.include_paths.iter() {
+        cc_build.flag(format!("-I{}/..", incl_path.display()));
+        cc_build.flag(format!("-I{}", incl_path.display()));
     }
 
     cc_build.compile("librcmaesglue.a");
-    println!("cargo:rustc-link-lib=dylib=cmaes");
 }
